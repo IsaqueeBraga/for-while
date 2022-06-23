9375715ea9318d81c5d931e60cf363f1566cb994
Isaquee Braga <flaisaque@hotmail.com>
2022-06-23 11:46:51 -0300

    aula03

---
+++ script.js
@@ -0,0 +1,22 @@
+for(let i = 0; i <= 10; i ++){
+    console. log (i)
+}
+
+
+let decrescente = 10;
+while (1<=decrescente){
+    console. log (decrescente)
+    decrescente --
+}
+
+let impares = 1;
+while (100 >= impares){
+    console. log (impares)
+    impares = impares +2
+}
+
+let pares = 0;
+while (100>= pares){
+    console. log (pares)
+    pares = pares +2
+}
